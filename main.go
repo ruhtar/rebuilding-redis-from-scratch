b0e326d68938e01c7b3851e88cb532078cb4f3ee
ruhtar <arthur.amorim10@gmail.com>
2025-01-15 23:07:59 -0300

    adds test method to read bytes of a string

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -16,15 +19,17 @@ const (
 )
 
 func main() {
-	port := "7777"
+	port := "6379"
 	fmt.Println("Listening on port: " + port)
 
 	// Create a new server
-	l, err := net.Listen("tcp", ": "+port)
+	l, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	playingWithByteReads()
 
 	// Listen for connections
 	conn, err := l.Accept()
@@ -39,7 +44,6 @@ func main() {
 		buf := make([]byte, 1024)
 
 		// read message from client
-
 		_, err = conn.Read(buf)
 		if err != nil {
 			if err == io.EOF {
@@ -51,4 +55,29 @@ func main() {
 
 		conn.Write([]byte("+OK\r\n"))
 	}
+}
+
+func playingWithByteReads() {
+	input := "$5\r\nAhmed\r\n"
+	reader := bufio.Reader(*bufio.NewReader(strings.NewReader(input)))
+
+	firstByte, _ := reader.ReadByte()
+	if firstByte != '$' {
+		fmt.Println("Invalid type, expecting bulk strings only")
+		os.Exit(1)
+	}
+
+	secondByte, _ := reader.ReadByte()
+	size, _ := strconv.ParseInt(string(secondByte), 10, 64)
+
+	fmt.Println(size)
+
+	// consume /r/n
+	reader.ReadByte()
+	reader.ReadByte()
+
+	name := make([]byte, size)
+	reader.Read(name)
+
+	fmt.Println(string(name))
 }
